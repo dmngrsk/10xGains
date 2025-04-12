19d903d952d1c662ad3866c59f961ab914873f09
Damian Górski <me@dmngrsk.pl>
2025-04-12 13:36:49 +0200

    chore(linting): Configure eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestComponentComponent } from './test-component/test-component.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'txg-root',
+  imports: [RouterOutlet, TestComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/test-component/test-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'txg-test-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-component.component.html',
+  styleUrl: './test-component.component.scss'
+})
+export class TestComponentComponent {
+
+}
